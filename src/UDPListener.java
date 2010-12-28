19497a205a96c283f004474fe6805a7d449e74dd
firefly2442 <firefly2442@gmail.com>
2010-12-28 00:11:57 -0600

    UDPListener now sends out a reply after receiving a new computer

--- src/UDPListener.java
+++ src/UDPListener.java
@@ -3,6 +3,8 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.util.StringTokenizer;
+import java.util.UUID;
 import java.util.logging.Level;
 
 
@@ -29,21 +31,37 @@ public class UDPListener implements Runnable
 				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 				try {
 					socket.receive(receivePacket);
-					String version = new String(receivePacket.getData());
+					String received = new String(receivePacket.getData()).trim();
+					StringTokenizer tokens = new StringTokenizer(received, "/");
+					String version = tokens.nextToken();
+					String universalID = tokens.nextToken();
+					
 					InetAddress IPAddress = receivePacket.getAddress();
 					Log.log("Received UDP status from: " + IPAddress.toString() + " running version: " + version, Level.INFO);
 					
-					//if the computer is running the same version we are
+					//if the computer is running the same version we are and it's not ourselves sending the message...
 					if (version.equals(Binswarm.VERSION))
 					{
-						//if this is a new computer that is not in our list, send a reply
-						//if (Networking.computerInList(uuid);
-					
-						//otherwise, update timestamp, IP, and other information
+						//make sure we don't add messages from ourself to the listing
+						if (!version.equals(Preferences.uuid.toString()))
+						{
+							//if this is a new computer that is not in our list, send a reply
+							if (!Networking.computerInList(UUID.fromString(universalID)))
+							{
+								UDPBroadcast.sendBroadcast();
+								//and create a new computer and add it to the list
+								Networking.addComputer(UUID.fromString(universalID), IPAddress.toString());
+							}
+							else
+							{
+								//the computer already is in the list, so update timestamp, IP, and other information
+								Networking.updateList(UUID.fromString(universalID), IPAddress.toString());
+							}
+						}
 					}
 					else
 					{
-						Log.log("Received UDP status from: " + IPAddress.toString() + ", but they are running a different version.", Level.INFO);
+						Log.log("Received UDP status from: " + IPAddress.toString() + ", but they are running a different version: (" + version + ")", Level.INFO);
 					}
 					
 				} catch (IOException e) {
