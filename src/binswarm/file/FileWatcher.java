df098c333e0338926355f83fcb1f9abe65c3c55f
firefly2442 <firefly2442@gmail.com>
2011-05-30 00:44:04 -0500

    started file watcher thread

---
+++ src/binswarm/file/FileWatcher.java
@@ -0,0 +1,53 @@
+package binswarm.file;
+
+import java.util.logging.Level;
+
+import binswarm.Log;
+
+public class FileWatcher implements Runnable {
+
+	private boolean running = false;
+	private Thread WatcherThread = new Thread(this);
+	
+	public FileWatcher()
+	{
+		//Constructor
+		startWatching();
+	}
+	
+	public boolean isRunning()
+	{
+		return running;
+	}
+	
+	public void startWatching()
+	{
+		if (!running && !WatcherThread.isAlive()) {
+			running = true;
+			WatcherThread.start();
+		}
+		else {
+			Log.log("Tried to start watching filesystem but thread already running!", Level.SEVERE);
+		}
+	}
+	
+	public void stopWatching()
+	{
+		running = false;
+		while (WatcherThread.isAlive())
+		{
+			//busy wait until the thread finishes up
+		}
+	}
+	
+
+	public void run() {
+		Log.log("Started watching local filesystem for file changes.", Level.INFO);
+		
+		while (running)
+		{
+			
+		}
+	}
+
+}
