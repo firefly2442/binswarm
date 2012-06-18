3322b1d220b7b84649ffee3d680f499f36e08db4
firefly2442 <firefly2442@gmail.com>
2012-06-17 23:35:47 -0500

    print version number at beginning

--- src/binswarm/Binswarm.java
+++ src/binswarm/Binswarm.java
@@ -1,5 +1,7 @@
 package binswarm;
 
+import java.util.logging.Level;
+
 import binswarm.config.LocationSettings;
 import binswarm.config.Preferences;
 import binswarm.ui.GUI;
@@ -9,6 +11,7 @@ public class Binswarm {
 	public static final String VERSION = "0.1";
 
 	public static void main(String[] args) {
+		Log.log("Binswarm version: " + VERSION + " started.", Level.INFO);
 		Preferences prefs = new Preferences();
 		LocationSettings locations = new LocationSettings();
 		GUI theGUI = new GUI();
